283366b287888e62039d8bd3bff45455e3f58e04
Tom White <twwhite.pvt@gmail.com>
2025-09-24 20:17:41 +1200

    moved player functions to separate file

---
+++ src/player.c
@@ -0,0 +1,58 @@
+#include "player.h"
+#include "shared.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+void movePlayer(uint8_t dir) {
+    fprintf(stderr, "Player direction: %d\n", dir);
+
+    // raise the redraw flag
+    beebram[PLAYER + PLR_ROOM6_REDRAW2] &= 0b11111100;
+    beebram[PLAYER + PLR_ROOM6_REDRAW2] |= true;
+
+    // for now simply increment/decrement the player x or y
+    uint16_t x, y;
+    switch (dir) {
+    case PLRDIR_E:
+        beebram[PLAYER + PLR_PVIZDEF_LO] = ADPTR_DOGWALKR & 0xFF;
+        beebram[PLAYER + PLR_PVIZDEF_HI] = ADPTR_DOGWALKR >> 8;
+        x = beebram[PLAYER + PLR_X_LO] | (beebram[PLAYER + PLR_X_HI] << 8);
+        x++;
+        beebram[PLAYER + PLR_X_LO] = x & 0xFF;
+        beebram[PLAYER + PLR_X_HI] = x >> 8;
+        break;
+
+    case PLRDIR_W:
+        beebram[PLAYER + PLR_PVIZDEF_LO] = ADPTR_DOGWALKL & 0xFF;
+        beebram[PLAYER + PLR_PVIZDEF_HI] = ADPTR_DOGWALKL >> 8;
+        x = beebram[PLAYER + PLR_X_LO] | (beebram[PLAYER + PLR_X_HI] << 8);
+        x--;
+        beebram[PLAYER + PLR_X_LO] = x & 0xFF;
+        beebram[PLAYER + PLR_X_HI] = x >> 8;
+
+        break;
+    case PLRDIR_N:
+        beebram[PLAYER + PLR_PVIZDEF_LO] = ADPTR_DOGWALKU & 0xFF;
+        beebram[PLAYER + PLR_PVIZDEF_HI] = ADPTR_DOGWALKU >> 8;
+        y = beebram[PLAYER + PLR_Y_LO] | (beebram[PLAYER + PLR_Y_HI] << 8);
+        y--;
+        beebram[PLAYER + PLR_Y_LO] = y & 0xFF;
+        beebram[PLAYER + PLR_Y_HI] = y >> 8;
+        break;
+
+    case PLRDIR_S:
+        beebram[PLAYER + PLR_PVIZDEF_LO] = ADPTR_DOGWALKD & 0xFF;
+        beebram[PLAYER + PLR_PVIZDEF_HI] = ADPTR_DOGWALKD >> 8;
+        y = beebram[PLAYER + PLR_Y_LO] | (beebram[PLAYER + PLR_Y_HI] << 8);
+        y++;
+        beebram[PLAYER + PLR_Y_LO] = y & 0xFF;
+        beebram[PLAYER + PLR_Y_HI] = y >> 8;
+        break;
+    }
+
+    // set the player direction vector
+
+    // check the next tile a pixel of movement in that direction will land
+
+    // take appropriate action
+}
---
+++ src/player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <stdint.h>
+
+void movePlayer(uint8_t dir);
+
+#endif
