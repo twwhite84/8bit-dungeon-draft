4e5978ad1b210cc3abb59c1a688bbce354c8bc4e
Tom White <twwhite.pvt@gmail.com>
2025-10-04 17:41:25 +1300

    wall sliding 2

--- src/player.c
+++ src/player.c
@@ -8,7 +8,7 @@ bool _isWallCollision(uint8_t i, uint8_t j, uint8_t dir_x, uint8_t dir_y);
 
 bool playerLook() {}
 
-void movePlayer() {
+void OLDmovePlayer() {
 
     // get current bearing
     uint8_t dir_x = beebram[PLAYER + ME_DIRX4_DIRY4] >> 4;
@@ -20,7 +20,7 @@ void movePlayer() {
     uint8_t i0 = (y0 >> 3), i1 = i0;
     uint8_t j0 = (x0 >> 3), j1 = j0;
 
-    bool x_update = true, y_update = true;
+    bool x_blocked = false, y_blocked = false;
 
     if (dir_x != DIR_ZERO) {
         (dir_x == DIR_NEGATIVE) ? x1-- : x1++;
@@ -31,11 +31,11 @@ void movePlayer() {
             for (uint8_t j = j1; j < (j1 + 3); j++) {
                 uint8_t tid = beebram[CAMBUFFER + 40 * i + j];
                 if (tid >= TID_WALL_SQUARE && tid < TID_WALL_SQUARE + 4) {
-                    x_update = false;
+                    x_blocked = true;
                 }
             }
         }
-        if (!x_update)
+        if (x_blocked)
             x1 = x0;
     }
 
@@ -49,18 +49,18 @@ void movePlayer() {
             for (uint8_t j = j1; j < (j1 + 3); j++) {
                 uint8_t tid = beebram[CAMBUFFER + 40 * i + j];
                 if (tid >= TID_WALL_SQUARE && tid < TID_WALL_SQUARE + 4) {
-                    y_update = false;
+                    y_blocked = true;
                 }
             }
         }
     }
 
-    if (x_update) {
+    if (!x_blocked) {
         beebram[PLAYER + ME_X_LO] = x1 & 0xFF;
         beebram[PLAYER + ME_X_HI] = x1 >> 8;
     }
 
-    if (y_update) {
+    if (!y_blocked) {
         beebram[PLAYER + ME_Y_LO] = y1 & 0xFF;
         beebram[PLAYER + ME_Y_HI] = y1 >> 8;
     }
@@ -72,6 +72,72 @@ void movePlayer() {
     beebram[PLAYER + CE_ROOMID6_REDRAW2] &= 0b11111100;
     beebram[PLAYER + CE_ROOMID6_REDRAW2] |= true;
 }
+
+/*----------------------------------------------------------------------------*/
+
+void movePlayer() {
+
+    // get current bearing
+    uint8_t dir_x = beebram[PLAYER + ME_DIRX4_DIRY4] >> 4;
+    uint8_t dir_y = beebram[PLAYER + ME_DIRX4_DIRY4] & 0x0F;
+
+    // get the current coordinates
+    uint16_t x0 = beebram[PLAYER + ME_X_LO] | (beebram[PLAYER + ME_X_HI] << 8), x1 = x0;
+    uint16_t y0 = beebram[PLAYER + ME_Y_LO] | (beebram[PLAYER + ME_Y_HI] << 8), y1 = y0;
+
+    // CHECK X MOVEMENT
+    if (dir_x != DIR_ZERO) {
+        bool x_blocked = false;
+        (dir_x == DIR_NEGATIVE) ? x1-- : x1++; // potential update
+        uint8_t i1 = y1 >> 3;
+        uint8_t j1 = x1 >> 3;
+
+        for (uint8_t i = i1; i < (i1 + 3); i++) {
+            for (uint8_t j = j1; j < (j1 + 3); j++) {
+                uint8_t tid = beebram[CAMBUFFER + 40 * i + j];
+                if (tid >= TID_WALL_SQUARE && tid < TID_WALL_SQUARE + 4) {
+                    x_blocked = true;
+                }
+            }
+        }
+        if (x_blocked) // cancels potential update
+            x1 = x0;
+    }
+
+    // CHECK Y MOVEMENT
+    if (dir_y != DIR_ZERO) {
+        bool y_blocked = false;
+        (dir_y == DIR_NEGATIVE) ? y1-- : y1++; // potential update
+        uint8_t i1 = y1 >> 3;
+        uint8_t j1 = x1 >> 3;
+
+        for (uint8_t i = i1; i < (i1 + 3); i++) {
+            for (uint8_t j = j1; j < (j1 + 3); j++) {
+                uint8_t tid = beebram[CAMBUFFER + 40 * i + j];
+                if (tid >= TID_WALL_SQUARE && tid < TID_WALL_SQUARE + 4) {
+                    y_blocked = true;
+                }
+            }
+        }
+        if (y_blocked) // cancels potential update
+            y1 = y0;
+    }
+
+    // save the updated coordinates
+    beebram[PLAYER + ME_X_LO] = x1 & 0xFF;
+    beebram[PLAYER + ME_X_HI] = x1 >> 8;
+    beebram[PLAYER + ME_Y_LO] = y1 & 0xFF;
+    beebram[PLAYER + ME_Y_HI] = y1 >> 8;
+
+    // update the sprite container with its new coordinates
+    updateSpriteContainer(PLAYER);
+
+    // raise the redraw flag to let renderer know movement has taken place
+    beebram[PLAYER + CE_ROOMID6_REDRAW2] &= 0b11111100;
+    beebram[PLAYER + CE_ROOMID6_REDRAW2] |= true;
+}
+
+/*----------------------------------------------------------------------------*/
 
 void _movePlayer() {
 
